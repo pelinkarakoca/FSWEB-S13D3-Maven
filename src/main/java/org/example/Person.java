a74865243045bf5eaff76fcdbe4a450df78ccea8
Pelin Karakoca <karakocapelin@gmail.com>
2024-09-18 14:39:29 +0300

    Finished assignment

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    double height;
+    double weight;
+    String gender;
+    public Person(String firstName, String lastName, int age){
+        this.firstName= firstName;
+        this.lastName=lastName;
+        this.age = age;
+    }
+    public Person(String firstName, String lastName, int age, double height, double weight, String gender){
+        this(firstName, lastName, age);
+        this.height = height;
+        this.weight = weight;
+        this.gender =gender;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public boolean isTeen(){
+        return this.age >= 13 && this.age < 19;
+    }
+
+}
---
+++ src/main/java/org/example/Wall.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Wall {
+    double width;
+    double height;
+    public Wall( double width, double height){
+       this.width = width;
+       this.height = height;
+    }
+    public double getWidth(){
+        return this.width;
+    }
+    public double getHeight(){
+        return this.height;
+    }
+    public void setWidth(double width){
+        if(width < 0) this.width = 0;
+        if(width >= 0) this.width = width;
+
+
+    }
+    public void setHeight(double height){
+        if(height < 0) this.height = 0;
+        if(height >= 0) this.height = height;
+
+    }
+    public double getArea(){
+        return this.width * this.height;
+    }
+}
